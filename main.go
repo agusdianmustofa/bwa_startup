157cc74bfce0a8a81f3215a73f183a1125a7807c
agus mustofa <agusmustofa300@gmail.com>
2024-03-07 11:49:23 +0700

    Upload avatars endpoint

--- main.go
+++ main.go
@@ -18,6 +18,7 @@ func main() {
 	}
 	userRepository := user.NewRepository(db)
 	userService := user.NewService(userRepository)
+	userService.SaveAvatar(1, "images/1-profile.jpg")
 	userHandler := handler.NewUserHandler(userService)
 
 	router := gin.Default()
@@ -26,6 +27,7 @@ func main() {
 	api.POST("/users", userHandler.RegisterUser)
 	api.POST("/sessions", userHandler.Login)
 	api.POST("/email_checkers", userHandler.CheckEmailAvailability)
+	api.POST("/avatars", userHandler.UploadAvatar)
 
 	router.Run()
 }
